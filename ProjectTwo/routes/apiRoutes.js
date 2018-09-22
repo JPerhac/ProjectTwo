2215a2113a984d6a950d26aa2507f977e4a9759b
JMP <porcayojm@gmail.com>
2018-09-22 11:14:44 -0700

    updated app.js get request and commented sections

--- ProjectTwo/routes/apiRoutes.js
+++ ProjectTwo/routes/apiRoutes.js
@@ -3,9 +3,9 @@ var db = require("../models");
 module.exports = function (app) {
   // Get all examples
   app.get("/api/examples", function (req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-    //   res.json(dbExamples);
-    // });
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.json(dbExamples);
+    });
     var data = ["greens", "bananas"];
     res.json(data);
   });
