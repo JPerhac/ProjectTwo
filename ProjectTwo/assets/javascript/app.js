a019ec8e8ce5eddfa0f49e4925e0ede545b9b1c6
JMP <porcayojm@gmail.com>
2018-09-22 11:01:09 -0700

    updated get request

--- ProjectTwo/assets/javascript/app.js
+++ ProjectTwo/assets/javascript/app.js
@@ -1,10 +1,36 @@
 $(document).ready(function(){
     $('#meats').click(function(){
         alert("Hi");
-        // $.get( "/api/examples", function( data ) {
-        //     $( "#dataDump" ).html( data );
-        //     console.log(data);
-        //   });
-    })
-})
+        $.get( "/api/examples", function( data ) {
+            $( "#dataDump" ).html( data );
+            console.log(data);
+          });
+    });
+});
 
+$(document).ready(fuction() {
+    $(#'fish').click(function() {
+        $.get('/api/examples', function(data) {
+            $('#dataDump').html(data);
+            console.log(data);
+        });
+    });
+});
+
+$(document).ready(fuction() {
+    $(#'veggies').click(function() {
+        $.get('/api/examples', function(data) {
+            $('#dataDump').html(data);
+            console.log(data);
+        });
+    });
+});
+
+$(document).ready(fuction() {
+    $(#'dairy').click(function() {
+        $.get('/api/examples', function(data) {
+            $('#dataDump').html(data);
+            console.log(data);
+        });
+    });
+});
